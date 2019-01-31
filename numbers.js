c69ea2f279c61008de13d50e924677a06d2c339e
Yanjun He <yanjunhe@YanjundeMBP.fios-router.home>
2019-01-31 00:18:28 -0500

    jan24 added

---
+++ numbers.js
@@ -0,0 +1,26 @@
+var user = {
+    name: "Gefei Tian",
+    zip: "02151",
+    greet: function(txt){
+        console.log("Hello " + txt);
+    }
+}
+
+var sayHello = function(name){
+    console.log("Hello" + name);
+}
+
+sayHello("Eric");
+
+user.greet("Hahahaha");
+
+var checkNumber = function(int){
+    if(int > 100) {
+        console.log("Holy! thats a big number");
+    }
+    else[
+        console.log("Boring! just a small number")
+    ]
+}
+
+checkNumber(10000)
