d74046e9f78b2c2cb374d0939b22c66c72782586
tiangefei <tiangefei@gmail.com>
2019-02-21 02:41:02 -0500

    fix more bug

--- homework6/homework6.js
+++ homework6/homework6.js
@@ -1,8 +1,18 @@
 console.log("Hi");
 
-d3.csv("data/esea_master_dmg_demos.part1.csv", function(error, data) {
+d3.csv("../data/esea_master_dmg_demos.part1.csv", function(error, data) {
     data.forEach(function(d) {
         d.export = parseFloat(d.wp);
     });
     console.log("csv:", data);
 });
+
+console.log("Hi");
+
+function getData(num) {
+    d3.csv("../data/esea_master_dmg_demos.part1.csv"), function(error, data){
+        console.log(data[num]);
+    }
+}
+
+console.log("Hi");
