208c32e9426f0f0d8d3161ed156e692eba8f2e87
tiangefei <tiangefei@gmail.com>
2019-02-21 14:41:21 -0500

    hw6 done

--- homework6/homework6.js
+++ homework6/homework6.js
@@ -1,18 +1,50 @@
-console.log("Hi");
-
-d3.csv("../data/esea_master_dmg_demos.part1.csv", function(error, data) {
-    data.forEach(function(d) {
-        d.export = parseFloat(d.wp);
+function showData() {
+    console.clear;
+    d3.csv("../data/esea_master_dmg_demos.part1.csv", function(error, data) {
+        console.log(data);  
     });
-    console.log("csv:", data);
-});
-
-console.log("Hi");
-
-function getData(num) {
-    d3.csv("../data/esea_master_dmg_demos.part1.csv"), function(error, data){
-        console.log(data[num]);
-    }
 }
 
-console.log("Hi");
+function countAK() {
+    var count = 0;
+    d3.csv("../data/esea_master_dmg_demos.part1.csv", function(error, data) {
+        data.forEach(function(d) {
+            if(d.wp=="AK47") {
+                count += 1;
+            }
+        });
+        console.log(count);  
+    });
+}
+
+function countM4() {
+    var count = 0;
+    d3.csv("../data/esea_master_dmg_demos.part1.csv", function(error, data) {
+        data.forEach(function(d) {
+            if(d.wp=="M4A4") {
+                count += 1;
+            }
+        });
+        console.log(count);  
+    });
+}
+
+function showWP() {
+    d3.csv("../data/esea_master_dmg_demos.part1.csv", function(error, data) {
+        data.forEach(function(d) {
+            console.log(d.wp);
+        });
+    });
+}
+
+function headRate() {
+    var count = 0;
+    d3.csv("../data/esea_master_dmg_demos.part1.csv", function(error, data) {
+        data.forEach(function(d) {
+            if(d.hitbox == "Head") {
+                count += 1;
+            }
+        });
+        console.log(count + " shots are headshot, headshot rate: %" + count/759*100);
+    });
+}
